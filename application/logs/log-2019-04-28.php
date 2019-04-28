262d1753bebbbb15ea949d41b6177007b718f864
Shavi Suri <Shavisuri@gmail.com>
2019-04-29 02:04:07 +0530

    First commit

---
+++ application/logs/log-2019-04-28.php
@@ -0,0 +1,121 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-04-28 07:41:30 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 07:41:30 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 07:41:56 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 07:41:56 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 07:41:59 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 07:41:59 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 07:42:05 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 07:42:06 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 07:43:23 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 07:43:23 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 08:02:15 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 08:02:15 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 08:02:33 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 08:02:33 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 08:02:42 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 08:02:42 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 08:03:15 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 08:03:15 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 16:32:24 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 16:32:25 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 16:32:52 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 16:32:53 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 16:32:53 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 16:32:55 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 16:32:55 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 16:32:55 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 16:33:36 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 16:33:36 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 16:33:36 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 16:33:37 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 16:33:37 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 16:33:37 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 16:33:40 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 16:33:41 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 16:33:41 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 16:33:46 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 16:33:46 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 16:33:46 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 16:41:57 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 16:41:57 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 16:41:57 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 17:00:11 --> Severity: Warning --> file_get_contents(C:\wamp64\www\shop\application\views/templates\shop\\assets\imgs\white-bag.png): failed to open stream: No such file or directory C:\wamp64\www\shop\application\controllers\Loader.php 98
+ERROR - 2019-04-28 17:01:36 --> Severity: Warning --> file_get_contents(C:\wamp64\www\shop\application\views/templates\shop\\assets\imgs\white-bag.png): failed to open stream: No such file or directory C:\wamp64\www\shop\application\controllers\Loader.php 98
+ERROR - 2019-04-28 17:01:42 --> Severity: Warning --> file_get_contents(C:\wamp64\www\shop\application\views/templates\shop\\assets\imgs\white-bag.png): failed to open stream: No such file or directory C:\wamp64\www\shop\application\controllers\Loader.php 98
+ERROR - 2019-04-28 17:22:25 --> Could not find the language line "user_order_history"
+ERROR - 2019-04-28 17:26:35 --> Could not find the language line "user_order_history"
+ERROR - 2019-04-28 18:30:34 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:30:34 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:30:34 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:30:39 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:30:39 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:30:39 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:31:08 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:31:08 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:31:09 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:31:31 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:31:31 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:31:32 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:32:04 --> Mailer Error: SMTP connect() failed. https://github.com/PHPMailer/PHPMailer/wiki/Troubleshooting
+ERROR - 2019-04-28 18:32:14 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:32:14 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:32:14 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:32:18 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:32:19 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:32:19 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:32:21 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:32:21 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:32:21 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:32:23 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:32:23 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:32:23 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:35:59 --> Severity: Notice --> Undefined variable: arr C:\wamp64\www\shop\application\models\Public_model.php 520
+ERROR - 2019-04-28 18:37:21 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:37:21 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:37:21 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:37:23 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:37:23 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:37:23 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:38:02 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:38:03 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:38:03 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:38:25 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:38:25 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:38:25 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:38:27 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:38:27 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:38:27 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:38:48 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:38:48 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:38:48 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:47:42 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:47:43 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:47:43 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:47:43 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:47:44 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:47:44 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:57:55 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:57:55 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:57:55 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:57:56 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:57:56 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:57:56 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:57:59 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:57:59 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 18:57:59 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 20:04:50 --> Could not find the language line "user_order_history"
+ERROR - 2019-04-28 20:04:57 --> Could not find the language line "user_order_history"
+ERROR - 2019-04-28 20:05:24 --> Could not find the language line "user_order_history"
+ERROR - 2019-04-28 20:05:36 --> Could not find the language line "user_order_history"
+ERROR - 2019-04-28 20:07:36 --> Could not find the language line "user_order_history"
+ERROR - 2019-04-28 20:08:54 --> Could not find the language line "user_order_history"
+ERROR - 2019-04-28 20:09:22 --> Could not find the language line "user_order_history"
+ERROR - 2019-04-28 20:09:53 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 20:09:53 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 20:09:53 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 20:12:40 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 20:12:40 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 20:12:40 --> 404 Page Not Found: /index
+ERROR - 2019-04-28 20:13:47 --> Could not find the language line "user_order_history"
